c38d64a295e05a88d2365de0c71a5c9d2b42358c
David Pottage <david@chrestomanci.org>
2015-02-05 21:38:29 +0000

    Use the string library

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "github.com/chrestomanci/go-exercises/stringutil"
 
 func main() {
-    fmt.Println("Hello World (from go)")
+    suffix := "World";
+    fmt.Println(stringutil.Reverse("!oG ,olleH") + " " + suffix)
 }
