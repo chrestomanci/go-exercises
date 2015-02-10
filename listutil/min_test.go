3dd95d5cbc8a5ce840ce0263749f05fea9a8e2d3
David Pottage <david@chrestomanci.org>
2015-02-10 22:22:29 +0000

    Fix the unit test.

--- listutil/min_test.go
+++ listutil/min_test.go
@@ -4,20 +4,22 @@ import (
 	"testing"
 )
 
-// NB: Not working. Syntax errors.
+type case_ent struct {
+	in   []int
+	want int
+}
+
 func TestXYZ(t *testing.T) {
-	cases := []struct {
-		in []int, want int
-	}{
-		{ [0], 0 },
-		{ [3,2,1], 1 },
-		{ [-5,0,1], -1},
-    };
+	cases := []case_ent{
+		{[]int{0}, 0},
+		{[]int{3, 2, 1}, 1},
+		{[]int{-5, 0, 1}, -5},
+	}
 
 	for _, c := range cases {
 		got := Min(c.in)
 		if got != c.want {
-			t.Errorf("Min(%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("Min(%v) == %d, want %d", c.in, got, c.want)
 		}
 	}
 }
