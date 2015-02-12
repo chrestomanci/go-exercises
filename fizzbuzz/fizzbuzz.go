2810486b5f18a6900c33a7de706bb1579b6d4011
David.Pottage <d.pottage@cv-library.co.uk>
2015-02-12 08:04:53 +0000

    Alternative algorithom, using a map

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	//	by_loop()
+	by_map()
+}
+
+func by_loop() {
 	for i := 1; i <= 100; i++ {
 		if 0 != i%3 && 0 != i%5 {
 			fmt.Println(i)
@@ -16,6 +21,28 @@ func main() {
 			}
 
 			fmt.Println("") // print newline
+		}
+	}
+}
+
+func by_map() {
+	numbers := make(map[int]string)
+
+	for i := 0; i <= 100; i += 3 {
+		numbers[i] += "Fizz"
+	}
+
+	for i := 0; i <= 100; i += 5 {
+		numbers[i] += "Buzz"
+	}
+
+	for i := 1; i <= 100; i++ {
+		content, ok := numbers[i]
+
+		if ok {
+			fmt.Println(content)
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
